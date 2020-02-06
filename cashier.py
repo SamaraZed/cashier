25ee8e9179a41e52739062263a383c7e6c0d3fa1
Ayham Samara <Ayham.Samara@live.com>
2020-02-06 16:46:05 +0200

    Test OK

--- cashier.py
+++ cashier.py
@@ -1,5 +1,24 @@
 def main():
-	# write code here
+	items = []
+	isdone = True
+	while (isdone):
+		item =  input("Item (enter \"done\" when finished): ")
+		if "done" not in item.lower():
+			items.append({
+			"name": item ,
+			"price" : float(input("please enter the price of the item:  ")),
+			"quantity": int(input("please enter the quantity of item/s:  "))
+			})
+		else:
+			isdone = False
+	print("-------------------\nrecepit\n-------------------")
+	totalPrice = 0.0
+	if len(items) > 0:
+		for item in items:
+			totalItemPrice = item["price"] * item["quantity"]
+			print(str(item["quantity"]) + " " + item["name"] + " " + str(totalItemPrice)+"KD" )
+			totalPrice += totalItemPrice
+		print("-------------------\nTotal Price: {}KD".format(totalPrice))
 
 if __name__ == '__main__':
 	main()
